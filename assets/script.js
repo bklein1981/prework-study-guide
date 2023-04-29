67b82b720c4f097a3d1e11cb0d6522336eab2e8f
Brian Klein <bklein1981@gmail.com>
2023-04-28 20:11:27 -0500

    added vars and loops to project

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+var topics = ["HTML", "CSS", "Git",'JavaScript'];
+
+/*if (topic === 'HTML') {
+    console.log("Let's study HTML!")
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  };
+  */
+
+  var shapes = ["triangle", "square", "pentagon", "circle"];
+  console.log(shapes[1]);
+
+  for(var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+  }
+
+  for(var y = 0; y <topics.length; y++) {
+    console.log(topics[y]);
+  }
